63a4925b80c8a29e4cafaadb206cc5038e5d4b21
kadenzipfel <kaden.zipfel@hotmail.com>
2023-05-15 14:31:56 -0700

    Add test for FibonacciGenerator

--- test/FibonacciGenerator.t.sol
+++ test/FibonacciGenerator.t.sol
@@ -4,10 +4,32 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "foundry-huff/HuffDeployer.sol";
 
-contract CounterTest is Test {
+contract FibonTest is Test {
     address public fibonacciGenerator;
 
     function setUp() public {
         fibonacciGenerator = address(HuffDeployer.deploy("src/FibonacciGenerator.huff"));
+    }
+
+    function testFibonacci(uint256 n) public {
+        (, bytes memory result) = fibonacciGenerator.call("");
+        assertEq(result, abi.encode(n));
+    }
+
+    function fib(uint256 n) internal pure returns (uint256) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            uint256 a = 0;
+            uint256 b = 1;
+            for (uint256 i = 2; i <= n; i++) {
+                uint256 c = a + b;
+                a = b;
+                b = c;
+            }
+            return b;
+        }
     }
 }
